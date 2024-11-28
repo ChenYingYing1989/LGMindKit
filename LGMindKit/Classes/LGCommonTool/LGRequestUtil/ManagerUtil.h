2aa5cd1a1ae1698ae9c15589f1a1d7d931db29f5
BonnieChen <861488970@qq.com>
2024-11-28 16:41:58 +0800

    ss

--- LGMindKit/Classes/LGCommonTool/LGRequestUtil/ManagerUtil.h
+++ LGMindKit/Classes/LGCommonTool/LGRequestUtil/ManagerUtil.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2015年 leiyi. All rights reserved.
 //
 #import <Foundation/Foundation.h>
-#import "AFNetworking.h"
+#import <AFNetworking/AFNetworking.h>
 
 @interface ManagerUtil : NSObject
 
